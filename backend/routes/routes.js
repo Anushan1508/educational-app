b8dceb6250d42dd784bea376a529eead01724316
Anushan Santhirakumar <anushan1508@gmail.com>
2021-03-27 11:35:07 +0530

    node modules removed

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const route = express.Router()
 
-const services = require('../services/render')
+const services = require('../server/services/render')
 
 /**
  * @description Root Route
