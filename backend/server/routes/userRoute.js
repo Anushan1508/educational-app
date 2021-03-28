97254e0f41ec511a586bada93e1967dfdef513dc
Anushan Santhirakumar <anushan1508@gmail.com>
2021-03-29 04:45:43 +0530

    logout

--- backend/server/routes/userRoute.js
+++ backend/server/routes/userRoute.js
@@ -123,7 +123,14 @@ router.post("/login", async (req, res) => {
     }
 });
 
-
+// logout
+router.get('/logout', (req, res) => {
+    res.cookie('token', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    .send();
+});
 
 module.exports = router;
 
