625e267e58015272cbcf3b33b594eaf3b94aee09
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-29 18:49:24 +0530

    removed extra navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Container, Row} from 'react-bootstrap'
+import { Col, Container, Row, Form, FormControl, Button, NavDropdown } from 'react-bootstrap'
 import { NavLink, Route, Link } from 'react-router-dom';
 import { ImBooks } from 'react-icons/im';
 import Signin from './Signin'
@@ -9,10 +9,10 @@ import './Navbar.css'
 
 function Navbar() {
     return (
-        <>  
-            <Container fluid style={{ backgroundColor: '#7395AE', height: '100px', position: 'sticky', marginTop: '0.5rem' }}>
+        <>
+            <Container fluid style={{ backgroundColor: '#7395AE', height: '150px', position: 'sticky', marginTop: '0.5rem' }}>
                 <Row >
-                    <Col xs lg="5">
+                    <Col xs lg="6">
                         <Link to="/home">
                             <div className='navbar-logo'>
                                 <ImBooks style={{ width: '40px', height: '40px', marginRight: '1rem' }} />
@@ -20,20 +20,42 @@ function Navbar() {
                             </div>
                         </Link>
                     </Col>
-                    <Col xs lg="0.2">
-                        <Link to='/articles' className='nav-links'>Articles</Link>
+                    <Col xs lg='2.5'>
+                        <Form inline style={{ marginTop: '25px'}}>
+                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                            <Button variant="secondary">Search</Button>
+                        </Form>
+                    </Col>
+                    </Row>
+                    <Row>
+                    <Col className='nav-items'>
+                        <Link to='/videos' className='nav-links'>Articles</Link>
                     </Col>
                     <Col className='nav-items'>
                         <Link to='/videos' className='nav-links'>Videos</Link>
                     </Col>
                     <Col className='nav-items'>
+                        <Link to='/videos' className='nav-links'>Quizes</Link>
+                    </Col>
+                    <Col className='nav-items'>
                         <Link to='/discussion' className='nav-links'>Discussion</Link>
                     </Col>
-                    <Col  className='nav-items'>
+                    <Col className='nav-items'>
                         <Link to='/contact' className='nav-links'>Contact Us</Link>
                     </Col>
                     <Col>
-                        <br />
+                        <NavDropdown title="User" id="collasible-nav-dropdown" className='title'>
+                            <NavDropdown.Item href="#action/3.1">Dashboard</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.2">Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Grade</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Messages</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.5">Preferences</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.6">Log out</NavDropdown.Item>
+                        </NavDropdown>
+                    </Col>
+                    <Col>
                         <NavLink to='/signin'>
                             <button className='button'>Sign In</button>
                         </NavLink>
